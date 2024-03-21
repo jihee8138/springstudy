c47635acc7183c285c132371020559034ba5c845
jihee8138 <jihee8138@gmail.com>
2024-03-21 16:32:24 +0900

    240321

---
+++ 07_mock_scheduler_interceptor/src/main/java/com/gdu/prj07/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.gdu.prj07.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.gdu.prj07.aspect.MyAfterAspect;
+import com.gdu.prj07.aspect.MyAroundAspect;
+import com.gdu.prj07.aspect.MyBeforeAspect;
+
+@EnableAspectJAutoProxy // aspect 허용하는 annotation
+@Configuration  // 나는 bean annotation을 이용해 bean을 만들겠다
+public class AppConfig {
+  
+  @Bean  // Componnent 를 대체하는 코드
+  public MyAroundAspect myAroundAspect() {
+    return new MyAroundAspect();
+  }
+  
+  @Bean
+  public MyBeforeAspect myBeforeAspect() {
+    return new MyBeforeAspect();
+  }
+  
+  @Bean
+  public MyAfterAspect myAfterAspect() {
+    return new MyAfterAspect();
+  }
+
+}
