403c8c2a345a1b247e59ba44a463bee63391a313
jihee8138 <jihee8138@gmail.com>
2024-04-01 15:59:27 +0900

    240401

--- myapp/src/main/java/com/gdu/myapp/utils/MyJavaMailUtils.java
+++ myapp/src/main/java/com/gdu/myapp/utils/MyJavaMailUtils.java
@@ -10,7 +10,16 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@PropertySource(value = "classpath:email.properties")  // resources까지가 classpath이므로 파일명만 적어줘도 됨
 public class MyJavaMailUtils {
+  
+  @Autowired
+  // Environment : 애플리케이션 환경설정 파일(프로필, 프로퍼티 등)들 읽어서 처리해주는 인터페이스  
+  private Environment env;  
   
   public void sendMail(String to, String subject, String content) {
     
@@ -25,14 +34,15 @@ public class MyJavaMailUtils {
     Session session = Session.getInstance(props, new Authenticator() {
       @Override
       protected PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication("gmail", "password");
+        return new PasswordAuthentication(env.getProperty("spring.mail.username")
+                                        , env.getProperty("spring.mail.password"));
       }
     });
     
     try {
       // 메일 만들기 (보내는 사람 + 받는 사람 + 제목 + 내용)
       MimeMessage mimeMessage = new MimeMessage(session);
-      mimeMessage.setFrom(new InternetAddress("gmail", "myapp"));  // setFrom 등록 시 address 타입으로 등록
+      mimeMessage.setFrom(new InternetAddress(env.getProperty("spring.mail.username"), "myapp"));  // setFrom 등록 시 address 타입으로 등록
       mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to));  // 받는 사람은 사용자가 이메일을 정보로 받아올꺼다
       mimeMessage.setSubject(subject);  // 제목도 service 에서 받아올꺼다 
       // 내용도 service 에서 받아올꺼다
